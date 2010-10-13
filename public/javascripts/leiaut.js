55287f86748f44a94fbb8a380eb560bb5f963a40
César Salazar <i.am@cesarsalzar.mx>
2010-10-13 15:26:13 -0700

    Added canvas and canvas resize code

--- public/javascripts/leiaut.js
+++ public/javascripts/leiaut.js
@@ -5,25 +5,35 @@ $(function(){
 	$('#tools').css({'width': '240px', 'height': '480px'});
 	$('#map').css({'width': '150px', 'height': '100px'});
 	$('#deck').css({'width': '400px', 'height': '50px'});
-	$('#game_board').css({'width': '700px', 'height': '400px'});
-	$('.tape').css({'width': '100px', 'height': '100px'});
+	$('#game_board').css({'width': '800px', 'height': '600px'});
+	$('.tape').css({'width': '50px', 'height': '50px'});
+	
+	
+	//ToDo: Load image and create game_board
 	
 	var game_boardW = $('#game_board').width();
 	var game_boardH = $('#game_board').height();
+	var tapeW = $('.tape').width();
+	var tapeH = $('.tape').height();
 	
+	//Bind slider
 	$('#slider').slider({
 		value: 1,
 		min: 0.5,
-		max: 2,
+		max: 3,
 		step: 0.05,
 		slide: function(event, ui) {
-			$('#game_board').width(game_boardW* ui.value);
-			$('#game_board').height(game_boardH* ui.value);
+			$('#game_board').width((game_boardW-tapeW)* ui.value);
+			$('#game_board').height((game_boardH-tapeH)* ui.value);
 			updateLayout();
+			drawImageInCanvas();
 		}
 	});
-		
+	
+	//Update general layout positioning	
 	updateLayout();
+	drawImageInCanvas();
+	
 	
 	$(window).resize(function(){
 		updateLayout();
@@ -36,7 +46,7 @@ var update = true;
 var updateLayout = function(){
 	
 	mainW = $(window).width();
-	mainH = $(window).height() 
+	mainH = $(window).height();
 	
 	if(mainW > 720 && mainH > 200 || update == true){
 		
@@ -87,4 +97,23 @@ var updateGameBoard = function(){
 	tape_seX = game_boardX - ($('#tape_se').width()/2) + $('#game_board').width();
 	tape_seY = game_boardY - ($('#tape_se').height()/2) + $('#game_board').height();
 	$('#tape_se').css({'left': tape_seX + 'px','top': tape_seY + 'px'});
+}
+
+var drawImageInCanvas = function(){
+	
+	var w = $(game_board).width();
+	var h = $(game_board).height();
+	var canvas = $('#myCanvas').get(0);
+
+	canvas.width = w; 				
+	canvas.height = h; 
+
+	var context = canvas.getContext('2d');
+
+	img = new Image();				
+	img.onload = function(){									
+		context.drawImage(this,0,0,w,h);
+	};
+	img.src='/images/beauty.jpg';
+	
 }
